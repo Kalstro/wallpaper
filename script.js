12abe7694d4e7df85ab086416310d5449fd7e666
Kalstro <154255275+Kalstro@users.noreply.github.com>
2024-07-13 18:41:38 +0300

    Fixed script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 // Script 1
 let currentWallpaper = 1;
 let allWallpapers = 33; // Set your value here
+let disable_buttons = true; // Set your value here
 
 document.getElementById('prevWallpaper').addEventListener('click', function() {
     changeWallpaper(-1);
@@ -25,22 +26,12 @@ document.getElementById('wallpaper').style.backgroundImage = "url('img/1.png')";
 document.addEventListener('mousemove', function(event) {
     let control = document.getElementById('wallpaperControl');
     let nearControl = event.clientY < 50 || event.clientY > (window.innerHeight - 50);
-
-    if (nearControl) {
-        control.classList.add('visible');
-    } else {
-        control.classList.remove('visible');
-    }
+    if (disable_buttons == false){
+        if (nearControl) {
+            control.classList.add('visible');
+        } else {
+            control.classList.remove('visible');
+        }
+    };
+    if (diable_buttons) {};
 });
-
-// Script 3
-let disable_buttons = false;
-
-if (diable_button) {
-    document.getElementById('prevWallpaper').disabled = true;
-    document.getElementById('nextWallpaper').disabled = true;
-}
-else {
-    document.getElementById('prevWallpaper').enabled = false;
-    document.getElementById('nextWallpaper').enabled = false;
-}
